784093f4e84dec32d9df8b27bd5dc11289708c39
Owen Lewis <owen98lewis@gmail.com>
2025-06-26 15:11:27 +0100

    add xml

--- Api/Request/GetRandomParticipantRequest.cs
+++ Api/Request/GetRandomParticipantRequest.cs
@@ -1,6 +1,12 @@
 namespace Api.Request;
 
+/// <summary>
+/// request for a random participant
+/// </summary>
 public class GetRandomParticipantRequest
 {
+    /// <summary>
+    /// The participants to select from
+    /// </summary>
     public IEnumerable<string> Participants { get; init; } = [];
 }
--- Api/Response/GetRandomParticipantResponse.cs
+++ Api/Response/GetRandomParticipantResponse.cs
@@ -1,6 +1,12 @@
 namespace Api.Response;
 
+/// <summary>
+/// The random participant response
+/// </summary>
  public class GetRandomParticipantResponse
 {
+    /// <summary>
+    /// The name of the randomly selected participant
+    /// </summary>
     public string Name { get; init; } = string.Empty;
 }
